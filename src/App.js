9e97c2a2e45031c0939c2e2d461b61186c9f6da1
DanielDiSalvo <danieldi_salvo@hotmail.com>
2019-03-08 20:32:16 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+      // constructor() {
+      //   super()
+
+      // This binding is necessary to make `this` work in the callback if not using Arrow Function
+      // this.changeName=this.changeName.bind(this);
+
+     // }
+
+  state = {
+    name: 'Daniel'
+  }
+
+  changeName = (newName) => {
+    this.setState({
+      name: newName
+    })
+  }
+
+  // changeName() {
+  //   this.setState({
+  //     name: 'Hello There!'
+  //   })
+  // }
+
+
+  changeNameFromInput= (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>Events - React</h1>
+          {/* <button onClick={() => this.changeName('Not So Awesome Juan')}>Change Name Using Arrow Function</button> */}
+          <button onClick={this.changeName.bind(this, 'Awesome Daniel')}>Change Name Using Bind Function</button>
+        <h2>Two Way Data Binding</h2>
+          <input type='text' onChange={this.changeNameFromInput} value={this.state.name}/>
+        <div>
+          {this.state.name}
+        </div>
       </div>
     );
   }
